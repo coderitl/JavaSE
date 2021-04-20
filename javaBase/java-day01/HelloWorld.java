b260def91a87c6b769b43c31c23cdfa8d694181b
lovobin <3327511395@qq.com>
2021-04-20 16:33:00 +0800

    add:javaBase code

---
+++ javaBase/java-day01/HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld Java");
+        int num = 10;
+        // 定义数组
+        int[] array = {1, 2, 3, 4};
+        // 使用高阶函数 filter map reduce
+
+    }
+}
